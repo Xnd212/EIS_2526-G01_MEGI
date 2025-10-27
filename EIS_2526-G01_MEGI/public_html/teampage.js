9e9af4f867085578a6bc1d9bd7ab95cfc0ac3c66
DaBid173 <up202105976@g.uporto.pt>
2025-10-27 11:06:32 +0000

    Team Page (inicio) Renomeações

---
+++ EIS_2526-G01_MEGI/public_html/teampage.js
@@ -0,0 +1,31 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// Aguarda até que todo o conteúdo da página esteja carregado
+document.addEventListener("DOMContentLoaded", () => {
+  const popup = document.getElementById("hover-popup");
+  });
+
+// =============================
+// Notificações 
+// =============================
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bellBtn = document.querySelector('.icon-btn[aria-label="Notificações"]');
+  const popup = document.getElementById('notification-popup');
+
+  if (bellBtn && popup) {
+    bellBtn.addEventListener('click', (e) => {
+      e.stopPropagation(); // Não fecha imediatamente se clicado
+      popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', (e) => {
+      if (!popup.contains(e.target) && !bellBtn.contains(e.target)) {
+        popup.style.display = 'none';
+      }
+    });
+  }
+});

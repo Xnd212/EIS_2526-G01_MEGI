30eedaeb9c38617809879d87b40efbcc99aa073c
Xnd212 <marcoalexandrefp@gmail.com>
2025-11-10 15:22:23 +0000

    Popup in Items from collection page

--- EIS_2526-G01_MEGI/public_html/collectionpage.js
+++ EIS_2526-G01_MEGI/public_html/collectionpage.js
@@ -20,7 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-// Define os dados associados a cada card de coleção
+
+// Aguarda até que todo o conteúdo da página esteja carregado
+document.addEventListener("DOMContentLoaded", () => {
+  const popup = document.getElementById("hover-popup"); 
+
+// Define os dados associados a cada item
   const itemData = {
     "charizard": {
       item: "Champion's Path Charizard V (PSA 10)",
@@ -50,3 +55,44 @@ document.addEventListener("DOMContentLoaded", () => {
       image: 'images/3.png'
     }
   };
+  
+// Para cada card dos Items mostra o conteúdo do popup ao mover o rato sobre o card
+  document.querySelectorAll(".item-card").forEach(block => {
+    block.addEventListener("mousemove", e => {
+      const id = block.getAttribute("data-id");
+      const data = itemData[id];
+      if (!data) return;
+
+      // Gera o conteúdo do popup com base nos dados da coleção
+      popup.innerHTML = `
+  <div class="popup-content-flex">
+            
+    <!-- Texto à esquerda -->
+    <div class="popup-text">
+      <h3 class="popup-title">${data.item}</h3>
+      <p><strong>Price:</strong> ${data.price}</p>
+      <p><strong>Importance:</strong> ${data.importance}</p>
+      <p><strong>Item Type:</strong> ${data.item_type}</p>
+      <p><strong>Acquisition Date:</strong> ${data.date}</p>
+      <p><strong>Acquisition Place:</strong> ${data.place}</p>
+    </div>
+
+    <!-- Imagem + nome do produto + data -->
+    <div class="popup-image">
+      <img src="${data.image}" alt="${data.item}">
+    </div>
+  </div>
+`;
+
+
+      // Posiciona o popup próximo ao cursor do rato
+      popup.style.left = e.pageX + 20 + "px";
+      popup.style.top = e.pageY - 225 + "px";
+      popup.classList.add("active");
+    });
+      // Esconde o popup quando o rato sai do card
+      block.addEventListener("mouseleave", () => {
+      popup.classList.remove("active");
+    });
+  });
+});
